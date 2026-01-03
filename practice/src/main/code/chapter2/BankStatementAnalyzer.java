452a16cf9c75f970685f22ea35df1bf9e1aecba1
dk7648 <dk7648@korea.ac.kr>
2026-01-03 18:16:04 +0900

    feat: add MainApplication

--- practice/src/main/code/chapter2/BankStatementAnalyzer.java
+++ practice/src/main/code/chapter2/BankStatementAnalyzer.java
@@ -5,18 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 
-public class BankTransactionAnalyzerSRP {
-    private static final String RESOURCES = "src/main/resources/";
-    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+public class BankStatementAnalyzer {
+    private final String RESOURCES = "src/main/resources/";
 
-    public static void main(final String... args) throws IOException {
-        final String fileName = args[0];
-        analyze(fileName, bankStatementParser);
-    }
-    public static void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
         final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
 
@@ -24,9 +18,10 @@ public class BankTransactionAnalyzerSRP {
                 = bankStatementParser.parseLinesFrom(lines);
         final BankStatementProcessor bankStatementProcessor
                 = new BankStatementProcessor(bankTransactions);
+
         collectSummary(bankStatementProcessor);
     }
-    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
+    private void collectSummary(final BankStatementProcessor bankStatementProcessor) {
         System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
         System.out.println("The total for transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
         System.out.println("The total for transactions in FEBRUARY is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
