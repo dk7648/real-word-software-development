10ee720754c163f8feb905be3e4f71917891665d
dk7648 <dk7648@korea.ac.kr>
2026-01-03 18:05:05 +0900

    feat: add interface BankStatementParser

---
+++ practice/src/main/code/chapter2/BankStatementParser.java
@@ -0,0 +1,8 @@
+package main.code.chapter2;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
