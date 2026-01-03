a66414fbe2220a2079e3304673005d462db7a573
dk7648 <dk7648@korea.ac.kr>
2026-01-03 12:09:25 +0900

    feat: chapter2.BankTransactionByMonthAnalyzerSimple

--- practice/src/main/code/chapter2/BankTransactionByMonthAnalyzerSimple.java
+++ practice/src/main/code/chapter2/BankTransactionByMonthAnalyzerSimple.java
@@ -9,18 +9,21 @@ import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-public class BankTransactionAnalyzerSimple {
+public class BankTransactionByMonthAnalyzerSimple {
     public static final String RESOURCES = "src/main/resources/";
     public static void main(final String... args) throws IOException {
         final Path path = Paths.get(RESOURCES + args[0]);
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         for(final String line: lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if(date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
-
         System.out.println("The total for all transactions is " + total);
     }
 }
