6d70485d129583a7afbdfeeb886087ce85bc1f14
dk7648 <dk7648@korea.ac.kr>
2026-01-04 09:47:56 +0900

    feat: add chapter3.BankstatementProcessor

---
+++ practice/src/main/code/chapter3/BankStatementProcessor.java
@@ -0,0 +1,63 @@
+package main.code.chapter3;
+
+import main.code.chapter2.BankTransaction;
+
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankStatementProcessor {
+    private final List<BankTransaction> bankTransactions;
+
+    public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
+        this.bankTransactions = bankTransactions;
+    }
+
+    public double calculateTotalAmount() {
+        double total = 0;
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public double calculateTotalInMonth(final Month month) {
+        double total = 0;
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getDate().getMonth() == month) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public double calculateTotalInCategory(final String category) {
+        double total = 0;
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getDescription().equals(category)) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public List<BankTransaction> findTransactionsGraterThanEqual(final int amount) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getAmount() >= amount) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+    public List<BankTransaction> findTransactionsInMonth(final Month month) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getDate().getMonth() == month) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+}
